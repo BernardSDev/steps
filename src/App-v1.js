6fe8eedf360d5c709815bce9486cf0584ea48a9d
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-07-02 18:56:23 +0100

    Create Reusable Button

--- src/App-v1.js
+++ src/App-v1.js
@@ -6,6 +6,7 @@ export default function App() {
 	return (
 		<div>
 			<Steps />
+			<Steps />
 		</div>
 	)
 }
@@ -34,34 +35,19 @@ function Steps() {
 						<div className={step >= 2 ? 'active' : ''}>2</div>
 						<div className={step >= 3 ? 'active' : ''}>3</div>
 					</div>
-
-					<StepMessage step={step}>{messages[step - 1]}</StepMessage>
+					<p className="message">
+						Step {step}: {messages[step - 1]}
+					</p>
 					<div className="buttons">
-						<Button bgColor="#7050f2" textColor="#fff" onClick={handlePrevious}>
-							<span>👈</span> Previous
-						</Button>
-						<Button bgColor="#7050f2" textColor="#fff" onClick={handleNext}>
-							<span>👉</span> Next
-						</Button>
+						<button style={{ backgroundColor: '#7050f2', color: '#fff' }} onClick={handlePrevious}>
+							Previous
+						</button>
+						<button style={{ backgroundColor: '#7050f2', color: '#fff' }} onClick={handleNext}>
+							Next
+						</button>
 					</div>
 				</div>
 			)}
 		</div>
-	)
-}
-
-function StepMessage({ step, children }) {
-	return (
-		<div className="message">
-			<h3>Step {step}</h3> {children}
-		</div>
-	)
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-	return (
-		<button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>
-			{children}
-		</button>
 	)
 }
